ec4ab5512c0d72e7a974c6204f5d0d5be34dd7fc
thiagoleitedio@gmail.com <thiagoleitedio@gmail.com>
2021-06-05 01:16:32 -0300

    share

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,51 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * Classe de exemplo para o exercício da Aula 2 de Operadores lógicos e relacionais, Controle de fluxo e Blocos.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean b1 = true;
+        boolean b2 = false;
+        boolean b3 = true;
+        boolean b4 = false;
+
+        System.out.println("b1 && b2 " + (b1 && b2));
+        System.out.println("b1 && b3 " + (b1 && b3));
+
+        System.out.println("b2 || b3 " + (b2 || b3));
+        System.out.println("b2 || b4 " + (b2 || b4));
+
+        System.out.println("b1 ^ b3 " + (b1 ^ b3));
+        System.out.println("b4 ^ b1 " + (b4 ^ b1));
+
+        System.out.println(!b1);
+        System.out.println(!b2);
+
+        int i1 = 10;
+        int i2 = 5;
+        float f1 = 20f;
+        float f2 = 50f;
+        System.out.println("((i1 + i2) < (f2 - f1)) && true " + (((i1 + i2) < (f2 - f1)) && true));
+        System.out.println("(i1 > i2) || (f2 < f1) " + ((i1 > i2) || (f2 < f1)));
+
+        double salarioMensal = 11893.58d;
+        double mediaSalario = 10500d;
+
+        int quantidadeDependentes = 4;
+        int mediaDependentes = 2;
+
+        System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+
+        boolean salarioBaixo = salarioMensal < mediaSalario;
+        boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
+
+        System.out.println((salarioBaixo) && (muitosDependentes));
+
+        boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+        System.out.println("recebeAuxilio " + recebeAuxilio);
+
+    }
+}
